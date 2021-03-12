0933138a02b9e345697c6aac9b66e8b402d9ea56
Sergei Goncharov <rootkitlink@gmail.com>
2021-03-12 14:17:15 +0200

    fix: made working functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (typeof array === "undefined" || array.length === 0 ){return 0;}
+  else {
+      let min = array[0], i = 0;
+      for (i; i < array.length; i++){
+        if (min > array[i]){
+          min = array[i];
+        }
+      }
+      return min;
+    }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (typeof array === "undefined" || array.length === 0 ){return 0;}
+  else {
+      let max = array[0], i = 0;
+      for (i; i < array.length; i++){
+        if (max < array[i]){
+          max = array[i];
+        }
+      }
+      return max;
+    }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (typeof array === "undefined" || array.length === 0 ){return 0;}
+  else {
+    let sum = 0, i = array.length;
+    while (i--){
+      sum += array[i];
+    }
+        return (sum / array.length);
+  }
 }
